ef377dcd9d2b3e8537f7d8ba36816ade3681518f
joey <joey_qpb@hotmail.com>
2024-09-03 19:20:51 -0300

    cleaning

--- src/main/java/com/joey/DSHC/controller/CommentController.java
+++ src/main/java/com/joey/DSHC/controller/CommentController.java
@@ -6,11 +6,8 @@ import com.joey.DSHC.model.CommentModel;
 import com.joey.DSHC.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/comments")
