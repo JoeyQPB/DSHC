e1385f2fed31864606bf9dbdaebb093dce9456df
JoeyQPB <joey_qpb@hotmail.com>
2024-09-18 09:15:51 -0300

    flyway

--- src/main/java/com/joey/DSHC/db/DatabaseConnectionTest.java
+++ src/main/java/com/joey/DSHC/db/DatabaseConnectionTest.java
@@ -1,4 +1,40 @@
 package com.joey.DSHC.db;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
 public class DatabaseConnectionTest {
+
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Value("${spring.datasource.username}")
+    private String databaseUsername;
+
+    @Value("${spring.datasource.password}")
+    private String databasePassword;
+
+    @PostConstruct
+    public void testConnection() {
+        System.out.println("Testing database connection...");
+        System.out.println("URL: " + databaseUrl);
+        System.out.println("USER: " + databaseUsername);
+        System.out.println("PASSWORD: " + databasePassword);
+        try (Connection connection = DriverManager.getConnection(databaseUrl, databaseUsername, databasePassword)) {
+            if (connection != null) {
+                System.out.println("Successfully connected to the database!");
+            } else {
+                System.out.println("Failed to make connection to the database!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error while connecting to the database: " + e.getMessage());
+        }
+    }
 }
+
